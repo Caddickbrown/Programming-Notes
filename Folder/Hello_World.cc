78952d332c7b0cebabbc501225efff90827537b5
Caddickbrown <danielcaddickbrown@gmail.com>
2020-12-04 11:37:55 +0000

    LoginCheck

--- Folder/Hello_World.cc
+++ Folder/Hello_World.cc
@@ -1,4 +1,4 @@
 cout<<"Hello World"; //This Line prints Hello World
 
 /* This is the start of a multi line comment
-This is the end of the same multi line comment*/
+This is the end of the same multi line comment.*/
