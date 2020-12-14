184836fd01fdcf04d1be9ef55647f561ea679fe9
Caddickbrown <danielcaddickbrown@gmail.com>
2020-12-14 16:51:47 +0000

    ToDo

--- Guidebook/ToDo.cc
+++ Guidebook/ToDo.cc
@@ -1,4 +1,4 @@
-//Just a basic To Do List to ensure I don't miss anything - to be updated and deleted as I think of Stuff
+//Just a basic To Do List to ensure I don't miss anything - to be updated and deleted as I think of/complete things
 
 //Explain While loops in more detail
 //Improve the analogies throughout the guide
