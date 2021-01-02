88e29a61201e2c4b8ad9acadb58c2a506308c9c9
Caddickbrown <danielcaddickbrown@gmail.com>
2021-01-02 19:02:47 +0000

    Project 2: Spaceshop Interface

---
+++ Guidebook/Arduino.cc
@@ -0,0 +1,29 @@
+//The following would flash lights in series when pressed - the green LED would stay on otherwise
+int switchState = 0; //This sets the variable of "switchState" as 0
+void setup() {
+  //This code sets the pins up as either Inputs or Outputs - telling the board what to expect from them.
+  pinMode (2, INPUT); // Push Switch - set as an input
+  pinMode (3, OUTPUT); // green LED - set as an output
+  pinMode (4, OUTPUT); // red LED - set as an output
+  pinMode (5, OUTPUT); // red LED - set as an output
+}
+
+void loop() {
+  switchState = digitalRead(2); //
+
+if (switchState == LOW) { //if the button is not pressed
+  digitalWrite(3, HIGH); // green LED - set to on
+  digitalWrite(4, LOW); // red LED - set to off
+  digitalWrite(5, LOW); // red LED - set to off
+  }
+else { //if the button is pressed - toggle the LEDs
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+  delay(250); //wait for a quarter of  second
+  //toggle the LEDs
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  delay(250); //wait for a quarter of  second
+  }
+} //go back to the beginning of the loop
