2082a255f647608cf1016c19cd91a3e427275daa
Caddickbrown <danielcaddickbrown@gmail.com>
2021-01-13 15:09:37 +0000

    Intro and Comments

--- Guidebook/Dart.dart
+++ Guidebook/Dart.dart
@@ -1,1 +1,16 @@
-//Dart 
+//Dart
+//Intro
+/*Dart is used for App development on FLutter
+Flutter is a universal app framework for iOS, Android, and Web Apps*/
+
+//Hello World
+void main(){ //main() is a predefined method in Dart - and acts as an entry point to the application. void is the Return Type - and means we won't return anything. Dart needs these to run.
+  print("Hello World!"); //Fairly straightforwards - this prints whatever is within the brackets
+}
+//This will output "Hello World!" You'll notice the syntax is similar to C, C++, and Java
+
+//Comments
+//These comments are exactly the same as C++
+//Single Line - Use "//"
+/*Multi-Line - Use "/*" to start. This is the start of a multi line comment
+This is the end of the same multi line comment.*/
