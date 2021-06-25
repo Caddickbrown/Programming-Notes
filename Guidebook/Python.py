55c0e79893b40601f8e326d3d3abe9034c86cc5a
Caddickbrown <danielcaddickbrown@gmail.com>
2021-06-25 13:42:41 +0100

    Python Outline

--- Guidebook/Python.py
+++ Guidebook/Python.py
@@ -5,6 +5,24 @@
 #Basic Symbols/Characters
 
 #Comments
-#Single Line - Use "#"
+#Single Line - Use "#" at the start of each line.
 #Multi-Line - Use "#" at the start of each line.
 #There aren't really ways to do Multi-line comments otherwise
+
+#Variables
+#Used to store data
+
+#Operators
+#Used to assign, compare and add values etc.
+
+#Control Flow
+#Conditional statements to create order code will be execused (Think IFELSE Statements)
+
+#Functions
+#Reusable codeblocks that perform a specific action when called
+
+#Data Types
+#How different types of data Numeric, Sequence, mapping booleans and binary types actually work
+
+#Loops
+#Used to execute code multiple types under a certain condition
