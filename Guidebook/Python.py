9d974b9ab05df93b6c114f5fedd1f736e4fbed3e
Caddickbrown <danielcaddickbrown@gmail.com>
2021-06-25 13:52:09 +0100

    Outline: The Second - The Return of the Outline

--- Guidebook/Python.py
+++ Guidebook/Python.py
@@ -9,20 +9,36 @@
 #Multi-Line - Use "#" at the start of each line.
 #There aren't really ways to do Multi-line comments otherwise
 
-#Variables
-#Used to store data
+#Basics
 
-#Operators
-#Used to assign, compare and add values etc.
+    #Variables
+    #Used to store data
 
-#Control Flow
-#Conditional statements to create order code will be execused (Think IFELSE Statements)
+    #Operators
+    #Used to assign, compare and add values etc.
 
-#Functions
-#Reusable codeblocks that perform a specific action when called
+    #Control Flow
+    #Conditional statements to create order code will be execused (Think IFELSE Statements)
 
-#Data Types
-#How different types of data Numeric, Sequence, mapping booleans and binary types actually work
+    #Functions
+    #Reusable codeblocks that perform a specific action when called
 
-#Loops
-#Used to execute code multiple types under a certain condition
+    #Data Types
+    #How different types of data Numeric, Sequence, mapping booleans and binary types actually work
+
+    #Loops
+    #Used to execute code multiple types under a certain condition
+
+#Object Oriented Programming
+
+    #Creating a class
+
+    #How Classes Work
+
+    #Creating Instance Variables
+
+    #How to use methods
+
+    #How inheritance works
+
+#How to work with Data Structures
