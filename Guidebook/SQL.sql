e176dec9225cd3aae51b3d811d0da78907be9653
Caddickbrown <danielcaddickbrown@gmail.com>
2021-08-19 13:10:32 +0100

    SQL Addition

---
+++ Guidebook/SQL.sql
@@ -0,0 +1,7 @@
+--Introduction
+--SQL is a language used for communicating with a database. It is the standard language for Relational Databases
+
+--Comments
+--Single Line - Use "--"
+/*Multi-Line - Use "/*" to start. This is the start of a multi line comment
+This is the end of the same multi line comment.*/
