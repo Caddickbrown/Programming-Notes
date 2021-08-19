ca90ef15d1ad464f582520631a76f557208cdf2e
Daniel Caddick-Brown <danielcaddickbrown@gmail.com>
2021-08-19 21:35:05 +0100

    Swift Starter

--- Guidebook/Swift.swift
+++ Guidebook/Swift.swift
@@ -1,6 +1,8 @@
 //Introduction
 //Swift was built for iOS/Apple development
-//It is used for:
+/*It is used for:
+iOS Development
+Mac App Development*/
 
 //Basic Symbols/Characters
 
@@ -8,3 +10,7 @@
 //Single Line - Use "//"
 /*Multi-Line - Use "/*" to start. This is the start of a multi line comment
 This is the end of the same multi line comment.*/
+
+//Commands
+//Commands have no spaces between them and they always end in brackets. Here is an example:
+thisIsCommand()
