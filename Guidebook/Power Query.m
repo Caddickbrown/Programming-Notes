f653ee34db2579a7ae31ebd473aa39f62daef0d9
Caddickbrown <danielcaddickbrown@gmail.com>
2022-05-26 14:43:12 +0100

    SQL Starter

---
+++ Guidebook/Power Query.m
@@ -0,0 +1,7 @@
+//Introduction
+//Power Query is a data transformation and data preparation engine. Power Query comes with a graphical interface for getting data from sources and a Power Query Editor for applying transformations. The M language is the data transformation language of Power Query.
+
+//Comments
+//Single Line - Use "//"
+/*Multi-Line - Use "/*" to start. This is the start of a multi line comment
+This is the end of the same multi line comment.*/
--- Guidebook/SQL.sql
+++ Guidebook/SQL.sql
@@ -1,7 +1,38 @@
 --Introduction
---SQL is a language used for communicating with a database. It is the standard language for Relational Databases
+--SQL (Structured Query Language) is a language used for communicating with a database. It is the standard language for Relational Databases
 
 --Comments
 --Single Line - Use "--"
 /*Multi-Line - Use "/*" to start. This is the start of a multi line comment
 This is the end of the same multi line comment.*/
+
+--DBMS
+/*The DBMS (Database Management System) manages the Database (Collection of Data files). It is the "Librarian" to the "Library of Data" and helps manage and organise everything efficiently.
+The DBMS is a collection of programs which enable users to:
+  - Create a Database
+  - Access the Database
+  - Manipulate the Data
+  - Protect and Secure the Data within the Database
+
+An RDBMS (Relational Database Management System) contains tables of information which have a relation between different tables.
+A program can be both DBMS and RDBMS - as long as it can store data in files and tables.
+
+Tables contain Rows (/Records) and Columns. Columns define the Data that the Rows under it must hold.*/
+
+--Entity Relationship (ER)
+/*Entity Relationship (ER) is the structured representation of a database with the help of a diagram.
+An Entity can be a place, person, object with a conceptual existence (college, job, course etc.), etc.
+If "Student" is the "Entity", then "Student ID, Name, Age, etc. would be the "Attributes" describing the Entity.
+First identify the Entity, Use an ER Diagram to define the Attributes before converting the diagram to a table. ER Diagrams are purely for reference and enable better planning of data management.
+ER Diagrams are just a visual representation of the database used by designers.
+
+ER Diagrams are used to identify:
+  - Entity - A Larger block about which you want to store data (Employee)
+  - Attributes - A column name and the fields that fall under it (Employee I.D., Job Title, Start Date etc.)
+  - Relationships - The link between 2 tables*/
+
+--Attributes
+/*Attributes are the properties of the Entity, and the "Variables" of each entry.*/
+
+--Super Sets vs Sub-Sets
+/*All tables under the RDBMS are interrelated.*/
